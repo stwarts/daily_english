f440ab6004d49cb7e717fea6bfe0b892eae737c7
stewart <npcuong.011308@gmail.com>
2020-02-09 23:23:03 +0700

    Add link option

--- assets/notification.js
+++ assets/notification.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+  Push.config({ serviceWorker: "./js/push/serviceWorker.min.js" });
+
   var todayWordAppUrl = 'https://script.googleusercontent.com/macros/echo?user_content_key=L9_Dwf3Cokmuqikrn6idlwBnyasW-oct5paAem8WcmEE-3QdQyz0T0SwMoRsqNVUuH9Ep3fgeRuv7_k_MJLngwkNLssVPuIim5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMkVWVuhZ_Ji_EDpXzjFnhu_Mr0dzpsTYKImT7wl107dR215DsJXeljVUVBnrrGuSN9SeJxDBnhlQWnTxpm2YOUUMTLGEaH11Q&lib=MLQcu4JvbhZZsWmuFIMFbPQqLiycxbmrY';
   var s = document.createElement('script');
   s.src = todayWordAppUrl;
@@ -6,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function handleTodayWord(todayWordObject) {
-  var toDisplayedWord = todayWordObject.word + ':' + todayWordObject.description;
-  Push.create('Your word today', { body: toDisplayedWord });
+  if (todayWordObject.word && todayWordObject.word.length) {
+    Push.create(todayWordObject.word, { body: "It's your new word today", link: '/daily_english?' + new URLSearchParams(todayWordObject).toString() });
+  }
 }
