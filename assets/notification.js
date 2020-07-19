230903ea5a3e081d79730075f63284738b32f02c
stewart <npcuong.011308@gmail.com>
2020-07-19 14:06:51 +0700

    Show word in html

--- assets/notification.js
+++ assets/notification.js
@@ -1,16 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
   var queryStringObject = convertToHash(location.search.substring(1));
   if (queryStringObject.word && queryStringObject.word.length) {
-    document.getElementById('word').innerHTML = queryStringObject.word;
-
-    var googleWordAnchor = document.getElementById('search-google');
-    googleWordAnchor.href = 'https://www.google.com/search?q=' + encodeURIComponent(queryStringObject.word);
-    googleWordAnchor.innerHTML = 'Google it';
-
-    document.getElementById('description').innerHTML = queryStringObject.description || '';
+    showTodayWord(queryStringObject);
   }
   else {
-    var todayWordAppUrl = 'https://script.googleusercontent.com/macros/echo?user_content_key=L9_Dwf3Cokmuqikrn6idlwBnyasW-oct5paAem8WcmEE-3QdQyz0T0SwMoRsqNVUuH9Ep3fgeRuv7_k_MJLngwkNLssVPuIim5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMkVWVuhZ_Ji_EDpXzjFnhu_Mr0dzpsTYKImT7wl107dR215DsJXeljVUVBnrrGuSN9SeJxDBnhlQWnTxpm2YOUUMTLGEaH11Q&lib=MLQcu4JvbhZZsWmuFIMFbPQqLiycxbmrY';
+    var todayWordAppUrl = 'https://script.googleusercontent.com/macros/echo?user_content_key=KkWw4oUBzm8nsVr3D7FbYOUR2F9A9YoOxUvAtZCy2PcUmWT3P4zxoNK9Q9DpZrf-PZ6EzPt3IxpzYt3BoZhTGRnt2x36vZ6om5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnMkVWVuhZ_Ji_EDpXzjFnhu_Mr0dzpsTYKImT7wl107dR215DsJXelj4NjEoAWiFjw&lib=MLQcu4JvbhZZsWmuFIMFbPQqLiycxbmrY';
     var s = document.createElement('script');
     s.src = todayWordAppUrl;
     document.body.appendChild(s);
@@ -19,6 +13,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
 function handleTodayWord(todayWordObject) {
   if (todayWordObject.word && todayWordObject.word.length) {
+    showTodayWord(todayWordObject);
+
     Push.create(
       todayWordObject.word,
       {
@@ -48,4 +44,14 @@ function convertShallowObjectToQueryString(object) {
   }).join('&');
 
   return queryString;
+}
+
+function showTodayWord(wordObject) {
+  document.getElementById('word').innerHTML = wordObject.word;
+
+  var googleWordAnchor = document.getElementById('search-google');
+  googleWordAnchor.href = 'https://www.google.com/search?q=' + encodeURIComponent(wordObject.word);
+  googleWordAnchor.innerHTML = 'Google it';
+
+  document.getElementById('description').innerHTML = wordObject.description || '';
 }
